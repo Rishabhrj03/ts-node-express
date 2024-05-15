6da6a47df093f75e067a0a054350b54f64429479
rishabhrj03 <rishabhskjoshi@gmail.com>
2024-05-15 23:25:26 +0530

    eslint and prettier not fixed

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,10 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Express + TypeScript Server');
+app.get('/', (req, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
